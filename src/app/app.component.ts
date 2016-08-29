85b0665eb02849027aacc121d6342b364e0d2d9d
Christos Koutsiaris <koutsiaris@gmail.com>
2016-08-29 12:37:50 +0100

    basic setup from guide

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import '../../public/css/materialize.min.css';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html'
+})
+
+export class AppComponent { }
